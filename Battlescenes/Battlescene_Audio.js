d7996fadbce2f1f8e0393b748249d8874510af0c
midorisan338 <catokura98@gmail.com>
2019-01-29 20:25:48 +0900

    clean dir

--- Battlescenes/Battlescene_Audio.js
+++
@@ -1,8 +0,0 @@
-const battle_se_tap = "audio/se/sword.mp3";
-let battle_se_volume = 0.5;
-
-    function AudioTest(lernid_){
-      var  audiotest1 = new Audio(battle_se_tap);
-      audiotest1.volume = battle_se_volume;
-      audiotest1.play();
-    }
